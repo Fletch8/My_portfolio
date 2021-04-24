55bb1dc0af483eda01465b91a08e84870becfb77
fletch8 <stampsfj04@gmail.com>
2021-04-24 03:43:06 -0400

    added styling to profile page

--- main_app/urls.py
+++ main_app/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.about, name='about'),
     path('about/', views.about, name='about'),
     path('contact/', views.contact, name='contact'),
     path('projects/', views.projects, name='projects'),
